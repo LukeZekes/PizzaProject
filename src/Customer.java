a4deeb43671f7056636837e2232f60cb97cfe11d
LukeZekes <41444778+LukeZekes@users.noreply.github.com>
2021-11-29 13:35:18 -0500

    Javadocs,cosntructor/retriever 4 existing accounts

--- src/Customer.java
+++ src/Customer.java
@@ -6,22 +6,29 @@ public class Customer {
     public String firstName, lastName, phoneNum, email, fullAddress, streetAddress, city, state, ZIP, password, orderComments;
     public int id; //-1 if guest
     OrderItem[] cartArray;
-//    public static int numCustomers = 0;
-    /*private static boolean isInteger(String input) {
-        try {
-            Integer.parseInt(input);
-            return true;
-        }
-        catch(NumberFormatException e) {
-            return false;
-        }
-    }*/
-
-    private Customer(String fn, String ln, String pn, String e, String sa, String c, String s, String z, String pw, boolean isNew) throws IOException {
-        if(isNew) //Guest
-            id = incrementNumCustomers()[1];
-        else
+    
+    /**
+     * Private constructor used when creating a new account or creating a guest account.
+     * <b>DO NOT CALL TO CREATE A CUSTOMER!</b> Use <i>createAccount()</i> or <i>createGuestAccount()</i>.
+     * @param fn First name
+     * @param ln Last name
+     * @param pn Phone number
+     * @param e Email address
+     * @param sa Street Address
+     * @param c City
+     * @param s State
+     * @param z ZIP code
+     * @param pw Password
+     * @param isGuest True if this is only a guest account, false if this is a new account to be saved
+     * @throws IOException
+     */
+    private Customer(String fn, String ln, String pn, String e, String sa, String c, String s, String z, String pw, boolean isGuest) throws IOException {
+        if(isGuest) {
             id = -1;
+        }
+        else {
+            id = incrementNumCustomers()[0];
+        }
         cartArray = new OrderItem[0];
         firstName = fn;
         lastName = ln;
@@ -36,32 +43,133 @@ public class Customer {
         password = pw;
         orderComments = "";
     }
-    public static Customer CreateAccount(String fn, String ln, String pn, String e, String sa, String c, String s, String z, String pw) throws IOException {
-        Customer customer = new Customer(fn, ln, pn, e, sa, c, s, z, pw, true);
+    /**
+     * Private constructor used when retrieving an account from the database.
+     * <b>DO NOT CALL TO CREATE A CUSTOMER!</b> Use <i>retrieveAccount()</i> or <i>createGuestAccount()</i>.
+     * @param fn First name
+     * @param ln Last name
+     * @param pn Phone number
+     * @param e Email address
+     * @param sa Street Address
+     * @param c City
+     * @param s State
+     * @param z ZIP code
+     * @param pw Password
+     * @param existingID ID of customer's account
+     * @t
+     */
+    private Customer(String fn, String ln, String pn, String e, String sa, String c, String s, String z, String pw, int existingID) throws IOException {
+        cartArray = new OrderItem[0];
+        firstName = fn;
+        lastName = ln;
+        phoneNum = pn;
+        email = e;
+        streetAddress = sa;
+        city = c;
+        state = s;
+        ZIP = z;
+        fullAddress = String.format("%s,%s,%s,%s", streetAddress, city, state, ZIP);
+        password = pw;
+        orderComments = "";
+        id = existingID;
+    }
+    
+    /**
+     * Creates a new customer account and saves it to the database
+     * @param fn First name
+     * @param ln Last name
+     * @param pn Phone number
+     * @param e Email address
+     * @param sa Street address
+     * @param c City
+     * @param s State
+     * @param z ZIP
+     * @param pw Password
+     * @return A Customer object with the included information, that has been saved to the database with a unique ID 
+     * @throws IOException
+     */
+    public static Customer createAccount(String fn, String ln, String pn, String e, String sa, String c, String s, String z, String pw) throws IOException {
+        Customer customer = new Customer(fn, ln, pn, e, sa, c, s, z, pw, false);
         try {
-            SaveCustomer(customer);
+            saveCustomer(customer);
         } catch (IOException ioException) {
             System.out.println(ioException.getMessage());
         }
         return customer;
     }
-    public static Customer CreateGuestAccount(String fn, String ln, String pn, String sa, String c, String s, String z) throws IOException {
-        return new Customer(fn, ln, pn, null, sa, c, s, z, null, false);
+    /**
+     * Creates a guest customer account 
+     * @param fn First name
+     * @param ln Last name
+     * @param pn Phone number
+     * @param sa Street address
+     * @param c City
+     * @param s State
+     * @param z ZIP
+     * @return A Customer object with the included information, and an ID of -1
+     * @throws IOException
+     */
+    public static Customer createGuestAccount(String fn, String ln, String pn, String sa, String c, String s, String z) throws IOException {
+        return new Customer(fn, ln, pn, null, sa, c, s, z, null, true);
     }
-
-    private static void SaveCustomer(Customer customer) throws IOException {
+ 
+    /**
+     * Searches for an account in the database using a phone number and password
+     * @param phoneNum
+     * @param password
+     * @return A Customer object with the account's information and ID
+     * @throws Exception if no customer is found with that information
+     */
+    public static Customer retrieveAccount(String phoneNum, String password) throws Exception {
+        //Locate customer
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        boolean found = false;
+        String line;
+        reader.readLine();
+        while((line = reader.readLine()) != null) {
+            String linePhoneNum = line.split(",")[3];
+            String linePassword = line.split(",")[5];
+            if(linePhoneNum.equals(phoneNum) && linePassword.equals(password)) {
+                found = true;
+                //Build Customer object from this data
+                break;
+            }
+        }
+        reader.close();
+        if(found) {
+            //Build a customer object from this data
+            System.out.println(line);
+            String[] data = line.split(",");
+            Customer customer = new Customer(data[1], data[2], data[3], data[4], data[6], data[7], data[8], data[9], data[5], Integer.parseInt(data[0]));
+            return customer;
+        }
+        else {
+            throw new Exception("Customer with phone number " + phoneNum + " not found!");
+        }
+    }
+    /**
+     * Saves a Customer to the database as a string in the format:
+     * <p>id,first name,last name,phone number,email,password,full address</p>
+     * @param customer The Customer object to be saved
+     * @throws IOException
+     */
+    private static void saveCustomer(Customer customer) throws IOException {
         StringBuilder sb = new StringBuilder();
         String info = String.format("%d,%s,%s,%s,%s,%s,%s", customer.id, customer.firstName, customer.lastName,
-                customer.phoneNum, customer.email, customer.password, customer.fullAddress, customer.password);
+                customer.phoneNum, customer.email, customer.password, customer.fullAddress);
         sb.append(info);
         sb.append("\n");
         BufferedWriter writer = new BufferedWriter(new FileWriter(path, true));
         writer.write(sb.toString());
         writer.close();
     }
-
-    //Returns true if there exists a customer with and id of customerID
-    public static boolean DeleteCustomer(int customerID) throws IOException {
+    /**
+     * Attempts to delete the account with the specified ID
+     * @param customerID The ID number of the account to delete
+     * @return True if successful, false if customer could not be found
+     * @throws IOException
+     */
+    public static boolean deleteAccount(int customerID) throws IOException {
         int matchID = customerID;
         boolean found = false;
         StringBuilder sb = new StringBuilder();
@@ -97,8 +205,13 @@ public class Customer {
         } else
             return false;
     }
-    //Increments the current and total number of customers and updates customers.txt to reflect it
-    //Returns an array, where [0] is current number of customers and [1] is total number
+    
+    /**
+     * Increments the current and total number of customers and updates customers.txt to reflect these new values.
+     * @return An array of integers, where [0] is the current number of stored customers and [1] is the total number of customers
+     * that have ever been stored
+     * @throws IOException
+     */
     private static int[] incrementNumCustomers() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
         StringBuilder sb = new StringBuilder();
@@ -124,14 +237,25 @@ public class Customer {
     }
     //Decrements the number of current customers and updates customers.txt to reflect it, won't go below zero
     //Returns an array, where [0] is current number of customers and [1] is total number
+
+    /**
+     * Decrements the number of current customers and updates customers.txt to reflect these new values.
+     * If for some reason either value attempts to go below zero, the file will be reset so that both values are zero and there are no accounts saved
+     * @return An array of integers, where [0] is the current number of stored customers and [1] is the total number of customers
+     * @throws IOException
+     */
     private static int[] decrementNumCustomers() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path));
         StringBuilder sb = new StringBuilder();
         String line;
         int[] numCustomers = getNumCustomers();
 
         if(numCustomers[0] <= 0 || numCustomers[1] <= 0) {
+            sb.append("0,0");
             reader.close();
+            bufferedWriter.write(sb.toString());
+            bufferedWriter.close();
             return new int[]{0, 0};
         }
 
@@ -148,13 +272,16 @@ public class Customer {
         }
         reader.close();
         //Override file
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path));
         bufferedWriter.write(sb.toString());
         bufferedWriter.close();
         return new int[]{numCustomers[0] - 1, numCustomers[1]};
     }
-    //Gets the current number of customers stored in customers.txt, and the total number of non-guest customers that have been stored (including deleted entires)
-    //getNumCustomers()[0] = current number of customers, getNumCustomers()[1] = total number of customers
+    /**
+     * Gets the <b>current</b> number of customers stored in customers.txt, and the <b>total</b> number of customers
+     * that have been stored (including deleted entires).
+     * @return an array of integers, where [0] = current number of customers,[1] = total number of customers
+     * @throws IOException
+     */
     private static int[] getNumCustomers() throws IOException {
         try {
             BufferedReader file = new BufferedReader(new FileReader(path));
